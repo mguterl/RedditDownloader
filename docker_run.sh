13150fb79fb883474fa308f6acd5782b9ae3128c
Michael Guterl <michael@diminishing.org>
2019-12-26 08:35:40 -0800

    Give container a name so it is easier to deal with while running

--- docker_run.sh
+++ docker_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-docker run \
+docker run -d -t \
+    --name redditdownloader \
     --mount type=bind,source="$(pwd)"/config,target=/config \
     --mount type=bind,source="$(pwd)"/downloads,target=/downloads \
     mguterl/redditdownloader
