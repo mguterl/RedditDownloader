589993e800d478ebdc593032572af1d67faca716
Michael Guterl <michael@diminishing.org>
2020-05-24 20:55:27 -0700

    Separate Dockerfile

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # DOCKER_BUILDKIT explanation: https://stackoverflow.com/a/55280541
-DOCKER_BUILDKIT=1 docker build -t mguterl/redditdownloader .
+DOCKER_BUILDKIT=1 docker build -f Dockerfile.mguterl -t mguterl/redditdownloader .
